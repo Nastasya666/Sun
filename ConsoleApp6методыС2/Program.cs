2331aa696f8770c772f41bcbc1adf037f75b6f4e
Nastasya666 <vd50_n.i.fomenko@mpt.ru>
2019-11-28 16:44:32 +0300

    sos

---
+++ ConsoleApp6методыС2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp6методыС2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Convert.ToString(Console.ReadLine());
+
+            if 
+        }
+        
+
+        
+    }
+}
